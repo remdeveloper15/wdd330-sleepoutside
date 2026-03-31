2e839c26ae446765e675af146cd1568dd512cc12
remdeveloper15 <helamanfranco@hotmail.com>
2026-03-31 14:17:15 -0500

    Week 04

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,14 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
+
+const myCheckout = new CheckoutProcess("so-cart", "#order-summary");
+myCheckout.init();
+
+document.querySelector("#zip").addEventListener("blur", myCheckout.calculateOrderTotal.bind(myCheckout));
+
+document.forms["checkout"].addEventListener("submit", (e) => {
+    e.preventDefault();
+    myCheckout.checkout(e.target);
+})
 
 loadHeaderFooter();
